97337dffcd6cfeeeece30a39a56ad513c8aafacc
kanottyan <wkjj3687@gmail.com>
2021-11-08 22:56:35 +0800

    snap lsit to be installed

---
+++ templates/ubuntu/snap.sh
@@ -0,0 +1,9 @@
+snap install git
+snap install ideaIU
+#vscode
+snap install code
+#airpos need blueman otherwise pulseaudio did not ditect the device by default in my case
+snap install blueman
+#web browser
+snap install brave
+
